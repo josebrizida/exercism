ae6048dcf547566d64c89d287830aa901368ec89
José Brízida <josejoaobrizida@gmail.com>
2017-04-13 16:39:17 +0100

    perfect numbers

--- ecmascript/perfect-numbers/perfect-numbers.js
+++ ecmascript/perfect-numbers/perfect-numbers.js
@@ -1,11 +1,35 @@
-class Gigasecond {
-    constructor (date) {
-        this.input = date
-    }
+"use strict";
 
-    date () {
-        return new Date(this.input.valueOf() + 1000000000000)
+function getFactors (n) {
+  let factors = []
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
+      factors.push(i)
     }
+  }
+  return factors
 }
 
-export default Gigasecond
+function getAliquotSum (n) {
+  return getFactors(n).reduce((acc, n) => acc + n, 0)
+}
+
+class PerfectNumbers {
+  classify (n) {
+    if (n <= 0) {
+      return 'Classification is only possible for natural numbers.'
+    }
+    let aliquotSum = getAliquotSum(n)
+    if (aliquotSum === n) {
+      return 'perfect'
+    }
+    if (aliquotSum > n) {
+      return 'abundant'
+    }
+    if (aliquotSum < n) {
+      return 'deficient'
+    }
+  }
+}
+
+export default PerfectNumbers
